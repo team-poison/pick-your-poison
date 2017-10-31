31b2b6046168fd1e785c22fa482fcf357831a0b2
Jason Young <jason@edgeofthebox.com>
2017-10-30 21:18:22 -0400

    created form for submission - still needs to be handlebarized to connect with database

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "joguktong1!",
+  socketPath:"/Applications/MAMP/tmp/mysql/mysql.sock", 
+
+  user: 'root',
+  password: 'root',
   database: "drinks_db"
 });
 
