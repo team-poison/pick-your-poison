5d3edcf5b7bc821a0df0e15772d8b4b8aa86ee83
coreankim@gmail.com <coreankim@gmail.com>
2017-11-11 00:08:00 -0500

    To Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,25 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-// <<<<<<< HEAD
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "joguktong1!",
-  database: "pyp_db"
-// =======
-//   socketPath:"/Applications/MAMP/tmp/mysql/mysql.sock", 
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+  // <<<<<<< HEAD
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "joguktong1!",
+    database: "pyp_db"
+  // =======
+  //   socketPath:"/Applications/MAMP/tmp/mysql/mysql.sock", 
 
-//   user: 'root',
-//   password: 'root',
-//   database: "drinks_db"
-// >>>>>>> jason
-});
+  //   user: 'root',
+  //   password: 'root',
+  //   database: "drinks_db"
+  // >>>>>>> jason
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
