2ada3f109a379674ff7e2bc32d977008e3bcd1ba
Zoli <zjsaunt@gmail.com>
2019-03-18 00:09:30 -0400

    try this?

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,33 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  var connection = mysql.createConnection({
-  // <<<<<<< HEAD
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "joguktong1!",
-    database: "pyp_db"
+// if (process.env.JAWSDB_URL) {
+
+
+var connection = mysql.createConnection(process.env.JAWSDB_URL,{
+  host: "yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "eqaqvvtuaacc451b",
+  password: "i64leo8n26rdrdvr",
+  database: "hcprff39kr3hkz24"
+});
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "pyp_db"
+// });
+
+
+// } else {
+//   var connection = mysql.createConnection({
+//   // <<<<<<< HEAD
+//     port: 3306,
+//     host: "localhost",
+//     user: "root",
+//     password: "joguktong1!",
+//     database: "pyp_db"
   // =======
   //   socketPath:"/Applications/MAMP/tmp/mysql/mysql.sock", 
 
@@ -18,8 +35,8 @@ if (process.env.JAWSDB_URL) {
   //   password: 'root',
   //   database: "drinks_db"
   // >>>>>>> jason
-  });
-};
+  // });
+// };
 
 // Make connection.
 connection.connect(function(err) {
